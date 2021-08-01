41931a17f399500db3af81476ff9f60e99cfa34a
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-08-01 12:51:58 +0300

    Retrofit Interface olusturuldu.

---
+++ app/src/main/java/com/noor/tmdbapp/data/api/TMDBService.kt
@@ -0,0 +1,33 @@
+package com.noor.tmdbapp.data.api
+
+import com.noor.tmdbapp.data.ArtistList
+import com.noor.tmdbapp.data.MovieList
+import com.noor.tmdbapp.data.TvShowList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TMDBService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query(
+            "api_key"
+        ) apiKey: String
+    ): Response<MovieList>
+
+    @GET("tv/popular")
+    suspend fun getPopularTvShows(
+        @Query(
+            "api_key"
+        ) apiKey: String
+    ): Response<TvShowList>
+
+    @GET("person/popular")
+    suspend fun getPopularArtists(
+        @Query(
+            "api_key"
+        ) apiKey: String
+    ): Response<ArtistList>
+
+}
